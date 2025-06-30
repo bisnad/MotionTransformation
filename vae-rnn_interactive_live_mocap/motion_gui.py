2888881b7987007e72f314066a31cc5f31b9bb70
bisnad <daniel.bisig@zhdk.ch>
2025-06-30 15:31:07 +0200

    changes the address part of the skeleton joint OSC messages to also include the skeleton index.

--- vae-rnn_interactive_live_mocap/motion_gui.py
+++ vae-rnn_interactive_live_mocap/motion_gui.py
@@ -137,8 +137,8 @@ class MotionGui(QtWidgets.QWidget):
         self.synth_pose_wrot_rh[:, 3] = self.synth_pose_wrot[:, 2]
 
         
-        self.sender.send("/mocap/joint/pos_world", self.synth_pose_wpos_rh)
-        self.sender.send("/mocap/joint/rot_world", self.synth_pose_wrot_rh)
+        self.sender.send("/mocap/0/joint/pos_world", self.synth_pose_wpos_rh)
+        self.sender.send("/mocap/0/joint/rot_world", self.synth_pose_wrot_rh)
 
     def update_pose_plot(self):
         
